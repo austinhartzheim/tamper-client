01a7eddab0bd2493c62bb10349ade953cda2d23b
Kristina Fischer <kmfischer3@wisc.edu>
2016-10-30 16:52:19 -0500

    cleanup

--- tamper-client.ino
+++ tamper-client.ino
@@ -1,43 +1,47 @@
 #include <Bridge.h>
 #include <HttpClient.h>
 
-int state = 0; //open
+#define STATE_OPEN 0
+#define STATE_CLOSED 1
+#define URL "https://<TODO>.amazonaws.com/prod/event/box_opened"
+
+int state = STATE_OPEN;
 
 void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(9600);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   Bridge.begin();
   Serial.begin(9600);
-  //while(!Serial);
 }
 
+void changeLED(int red, int green, int blue) {
+  analogWrite(3, red);
+  analogWrite(5, green);
+  analogWrite(6, blue);
+}
 void loop() {
-  // put your main code here, to run repeatedly:
   if (analogRead(A0) > 300) {
     // open - no magnet
-    if (state == 1) {
+
+    // send notification if previous state was closed
+    if (state == STATE_CLOSED) {
       HttpClient client;
       client.get("http://www.arduino.cc/asciilogo.txt");
-
       while (client.available()) {
-        //Serial.println("state changed from closed to open");
         char c = client.read();
         Serial.print(c);
       }
       Serial.flush();
     }
-    state = 0;
-    //Serial.println(".");
-    analogWrite(3, 0);
-    analogWrite(5, 100);
+    
+    state = STATE_OPEN;
+    changeLED(0, 100, 0);
+    
   } else {
     // closed - magnet
-    state = 1;
-    //Serial.println("=");
-    analogWrite(3, 100);
-    analogWrite(5, 0);
+    state = STATE_CLOSED;
+    changeLED(100, 0, 0);
   }
+  
   delay(10);
 }
