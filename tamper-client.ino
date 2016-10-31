8d16623efb2d7d29889ba994dd7bb64cb225d9a1
Kristina Fischer <kmfischer3@wisc.edu>
2016-10-30 22:21:40 -0500

    alert and ping notifications

--- tamper-client.ino
+++ tamper-client.ino
@@ -1,17 +1,32 @@
 #include <Bridge.h>
 #include <HttpClient.h>
+#include <stdio.h>
 #include "config.h"
 
 #define STATE_OPEN 0
 #define STATE_CLOSED 1
 
 int state = STATE_OPEN;
+int sessionID;
+char urlBuffer[URL_BUFFER_SIZE];
+unsigned long lastPing = millis(); 
+HttpClient client;
 
 void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   Bridge.begin();
   Serial.begin(9600);
+  randomSeed(analogRead(1));
+  sessionID = random(500);
+  ping();
+}
+
+void ping() {
+  snprintf(urlBuffer, 300, PING_URL, sessionID);
+  Serial.println(urlBuffer);
+  client.get(urlBuffer);
+  sessionID++;
 }
 
 void changeLED(int red, int green, int blue) {
@@ -19,20 +34,24 @@ void changeLED(int red, int green, int blue) {
   analogWrite(5, green);
   analogWrite(6, blue);
 }
+
 void loop() {
+
+  unsigned long currentTime = millis();
+  
   if (analogRead(A0) > 300) {
     // open - no magnet
     changeLED(0, 100, 0);
     
-    // send notification if previous state was closed
+    // send alert notification if box is opened
     if (state == STATE_CLOSED) {
-      HttpClient client;
-      client.get(URL);
+      client.get(ALERT_URL);
       while (client.available()) {
-        //Serial.print("client available");
         char c = client.read();
         Serial.print(c);
       }
+
+      // updating at the end to detect state transition
       state = STATE_OPEN;
       Serial.flush();
     }
@@ -40,6 +59,14 @@ void loop() {
   } else {
     // closed - magnet
     changeLED(100, 0, 0);
+
+    // begin ping notifications when box is locked
+    if ( state == STATE_OPEN || (currentTime-lastPing)>PING_INTERVAL ) {
+      ping();
+      lastPing = currentTime;
+    }
+    
+    // updating at the end to detect state transition
     state = STATE_CLOSED;
   }
   
