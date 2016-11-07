edf54924d47fb3cd80176af2b78d64935237e7b8
Kristina Fischer <kmfischer3@wisc.edu>
2016-11-06 21:59:27 -0600

    added timeouts to transition states

--- tamper-client.ino
+++ tamper-client.ino
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include "config.h"
 
-enum State {OPEN, TRANSITION1, TRANSITION2, TRANSITION3, CLOSED};
+#define TRANSITION_TIMEOUT 1500
+
+enum State {OPEN, TRANSITION1, TRANSITION2, TRANSITION3, CLOSED, WAIT_UNTIL_OPEN};
 
 State state = OPEN;
 State lastState = OPEN;
@@ -11,6 +13,7 @@ State lastState = OPEN;
 int sessionID;
 char urlBuffer[URL_BUFFER_SIZE];
 unsigned long lastPing = millis(); 
+unsigned long lastStateChange = millis(); 
 HttpClient client;
 
 void setup() {
@@ -20,15 +23,17 @@ void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(1));
   sessionID = random(500);
-  ping();
+  ping(true);
 }
 
-void ping() {
-  snprintf(urlBuffer, 300, PING_URL, sessionID);
-  Serial.println(urlBuffer);
-  client.get(urlBuffer);
-  sessionID++;
-  lastPing = millis();
+void ping(bool forcePing) {
+  if (forcePing || (millis()-lastPing)>PING_INTERVAL) {
+    snprintf(urlBuffer, 300, PING_URL, sessionID);
+    Serial.println(urlBuffer);
+    client.get(urlBuffer);
+    sessionID++;
+    lastPing = millis();    
+  } 
 }
 
 void changeLED(int red, int green, int blue) {
@@ -40,6 +45,7 @@ void changeLED(int red, int green, int blue) {
 void changeState(State newState) {
   lastState = state;
   state = newState;
+  lastStateChange = millis();
 }
 
 // returns true if magnet on lid is detected
@@ -57,8 +63,14 @@ void alarm() {
     }
 }
 
+void boxLocked() {
+  client.get(LOCKED_URL);
+  while (client.available()) {
+    client.read();
+  }
+}
+
 void loop() {
-  
   switch (state) {
       case OPEN:
         changeLED(0, 100, 0);
@@ -67,20 +79,27 @@ void loop() {
         }
         break;
       case TRANSITION1:
-        changeLED(0, 0, 50);
+        changeLED(0, 0, 25);
+        if (millis() - lastStateChange > TRANSITION_TIMEOUT) {
+          changeState(WAIT_UNTIL_OPEN);
+        }
         if (!isMagnetClosed()) {
           changeState(TRANSITION2);
         }
         break;
       case TRANSITION2:
         changeLED(0, 0, 100);
+        if (millis() - lastStateChange > TRANSITION_TIMEOUT) {
+          changeState(WAIT_UNTIL_OPEN);
+        }
         if (isMagnetClosed()) {
           changeState(TRANSITION3);
         }
         break;
       case TRANSITION3:
         changeLED(100, 0, 0);
-        ping();
+        ping(true);
+        boxLocked();
         changeState(CLOSED);      
         break;
       case CLOSED:
@@ -89,8 +108,15 @@ void loop() {
           alarm();
           changeState(OPEN);
         }
+        ping(false);
+        break;
+      case WAIT_UNTIL_OPEN:
+        changeLED(100,100,100);
+        if (!isMagnetClosed()) {
+          changeState(OPEN);
+        }
         break;
       default:
-      break;
+        break;
     }
 }
